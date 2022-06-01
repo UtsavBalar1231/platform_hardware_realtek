581e6e192469ef3a5ae073858a332bca3f12bcce
UtsavBalar1231 <utsavbalar1231@gmail.com>
2022-06-01 07:35:13 +0000

    rtkbt: libbt-vendor: Configure libbt build configuration for rtl8822cs

--- rtkbt/code/libbt-vendor/include/vnd_buildcfg.h
+++ rtkbt/code/libbt-vendor/include/vnd_buildcfg.h
@@ -1,7 +1,12 @@
 #ifndef _VND_BUILDCFG_H
 #define _VND_BUILDCFG_H
-#define BLUETOOTH_UART_DEVICE_PORT   "/dev/ttyS1"
-#define FW_PATCHFILE_LOCATION   "/etc/firmware/"
+#define BLUETOOTH_UART_DEVICE_PORT   "/dev/ttyS0"
+#define FW_PATCHFILE_LOCATION   "/vendor/etc/firmware/"
+#define VENDOR_LIB_CONF_FILE "/vendor/etc/bluetooth/bt_vendor.conf"
+#define USE_CONTROLLER_BDADDR TRUE
+#define UART_TARGET_BAUD_RATE 115200
+#define FW_PATCH_SETTLEMENT_DELAY_MS 200
+#define BT_WAKE_VIA_PROC	TRUE
 #define LPM_IDLE_TIMEOUT_MULTIPLE   5
 #define SCO_USE_I2S_INTERFACE   TRUE
 #define BTVND_DBG   TRUE
